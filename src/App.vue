c0aa0f8a1c7a5b2f12c1852a80af8993c4ba135d
Regis <dev05@sistemaeso.com.br>
2026-02-25 09:55:21 -0300

    Define Reactive State and Functions within a Composable

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,52 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useCycleList } from "./composables/useCycleList";
+
+const animals = ref(["Dog", "Cat", "Bird", "Iguana"]);
+
+const { state, prev, next, go } = useCycleList(animals);
+
+const goTo = ref(0);
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    {{ state }}
+    <br />
+    <button @click="prev">Previous</button>
+    <button @click="next">Next</button>
+    <br />
+
+    <input type="number" v-model="goTo" />
+    <button @click="go(goTo)">Go To {{ goTo }}</button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
--- src/composables/useCycleList.ts
+++ src/composables/useCycleList.ts
@@ -1,10 +1,41 @@
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import type { Ref } from "vue";
 
-export const useCycleList = (list: any[]) => {
+export const useCycleList = (list: any[] | Ref<any[]>) => {
+  const activeIndex = ref(0);
+
+  const state = computed(() => list[activeIndex.value]);
+
+  function next() {
+    if (activeIndex.value === list.length - 1) {
+      activeIndex.value = 0;
+    } else {
+      activeIndex.value++;
+    }
+  }
+
+  function prev() {
+    if (activeIndex.value === 0) {
+      activeIndex.value = list.length - 1;
+    } else {
+      activeIndex.value--;
+    }
+  }
+
+  function go(index: number) {
+    if (index >= list.length) {
+      throw new Error(
+        `Cannot go to index ${index}. The list provided to useCycleList is not that long.`,
+      );
+    } else {
+      activeIndex.value = index;
+    }
+  }
+
   return {
-    state: ref(""),
-    prev: () => {},
-    next: () => {},
-    go: () => {},
+    state,
+    prev,
+    next,
+    go,
   };
 };
